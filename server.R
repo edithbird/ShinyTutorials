487029bada1308f05aec187c30908eaa0aca475c
edithbird <edithbird5@gmail.com>
2017-03-07 22:35:22 -0500

    New shiny census vis

---
+++ server.R
@@ -0,0 +1,51 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(maps)
+library(mapproj)
+source("helpers.R")
+counties <- readRDS("data/counties.rds")
+
+shinyServer(
+  function(input, output) {
+    
+    output$map <- renderPlot({
+      
+      
+      
+      data <- switch(input$var,
+                     "Percent White"= counties$white, 
+                     "Percent Black" = counties$black, 
+                     "Percent Hispanic" = counties$hispanic, 
+                     "Percent Asian" = counties$asian)
+      
+      color <- switch(input$var,
+                      "Percent White" = "darkgreen",
+                      "Percent Black" = "red",
+                      "Percent Hispanic" = "darkorange",
+                      "Percent Asian" = "darkviolet")
+      
+      legend <- switch(input$var, 
+                       "Percent White" = "% White", 
+                       "Percent Black" = "% Black", 
+                       "Percent Hispanic" = "% Hispanic", 
+                       "Percent Asian" = "% Asian")
+      
+      
+      percent_map(var = data, 
+                  color = color, 
+                  legend.title = legend, 
+                   max = input$range[2], 
+                  min = input$range[1])
+    })
+      
+  }
+  )
+    
---
+++ ui.R
@@ -0,0 +1,33 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel("Census Vis"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Create demographic maps with 
+        information from the 2010 US Census."),
+      
+      selectInput("var", 
+                  label = "Choose a variable to display",
+                  choices = c("Percent White", "Percent Black",
+                              "Percent Hispanic", "Percent Asian"),
+                  selected = "Percent White"),
+      
+      sliderInput("range", 
+                  label = "Range of interest:",
+                  min = 0, max = 100, value = c(0, 100))
+    ),
+    
+    mainPanel(plotOutput("map"))
+  )
+))
